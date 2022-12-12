b0676b373a29332718b97310dde9fedaad8449d0
rjsej12 <rjsej12@naver.com>
2022-12-12 15:15:17 +0900

    feat: Add color theme in tailwind.config
    primary, sub, description 색 테마 추가

--- front/tailwind.config.cjs
+++ front/tailwind.config.cjs
@@ -2,7 +2,13 @@
 module.exports = {
   content: ['./*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#ea917e',
+        desc: '#454545',
+        sub: '#735e6f',
+      },
+    },
   },
   plugins: [],
 };
