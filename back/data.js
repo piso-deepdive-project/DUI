239b35cadc070e9f92a353e685bce7ca7f399bae
PisoDev77 <pisodev0427@gmail.com>
2022-12-12 16:51:34 +0900

    feta: REST API post
    - 글 목록 가져오기
    - getPosts
    
    - 글 등록하기
    - addPost
    
    - 글 가져오기
    - getPost
    
    - 글 수정하기
    - updatePost
    
    - 글 삭제하기
    - deletePost

--- back/data.js
+++ back/data.js
@@ -1,25 +1,45 @@
 const users = [
   {
-    id: "test123@gmail.com",
-    pwd: "utzazz12!!",
-    author: "Uta",
+    id: 'test123@gmail.com',
+    pwd: 'utzazz12!!',
+    author: 'Uta',
   },
 ];
 
-const getUsers = () => users;
-
-const posts = [
+let posts = [
   {
     id: 0,
-    title: "TEST POST TITLE",
-    author: { id: "test123@gmail.com", pwd: "utzazz12!!", author: "Uta" },
+    title: 'TEST POST TITLE',
+    author: { id: 'test123@gmail.com', pwd: 'utzazz12!!', author: 'Uta' },
     tags: [],
-    date: new Date("2022-10-08"),
+    date: new Date('2022-10-08'),
   },
 ];
 
+// POST
 const getPosts = () => posts;
+
+const getPost = id => posts.filter(post => post.id === id);
+
+const addPost = post => {
+  posts = [...posts, post];
+};
+
+const updatePost = post => {
+  posts = posts.map(_post => (_post.id === +post.id ? post : _post));
+  return getPost(+post.id);
+};
+
+const deletePost = id => {
+  posts = posts.filter(post => post.id !== id);
+  console.log(posts);
+  return getPost(id);
+};
 
 module.exports = {
   getPosts,
+  getPost,
+  addPost,
+  updatePost,
+  deletePost,
 };
--- back/index.js
+++ back/index.js
@@ -1,20 +1,53 @@
-const { getPosts } = require("./data.js");
-
-const express = require("express");
+const express = require('express');
+const { getPosts, addPost, getPost, updatePost, deletePost } = require('./data');
 
 // const { Signin } = require('./api/sign/signin');
 
 const server = express();
 const PORT = 6000;
 
-server.use(express.static("public"));
+server.use(express.static('public'));
 server.use(express.json());
 
 // Signin(server);
 
 // POST API
-server.get("/posts", (req, res) => {
+/**
+ * 글 목록 가져오기
+ */
+server.get('/posts', (req, res) => {
   res.send(getPosts());
+});
+
+/**
+ * 글 가져오기
+ */
+server.get('/post/:id', (req, res) => {
+  res.send(getPost(+req.params.id));
+});
+
+/**
+ * 글 추가
+ */
+server.post('/post', (req, res) => {
+  const post = req.body;
+  addPost(post);
+  res.send(post);
+});
+
+/**
+ * 글 수정하기
+ */
+server.patch('/post', (req, res) => {
+  const post = req.body;
+  res.send(updatePost(post));
+});
+
+/**
+ * 글 삭제하기
+ */
+server.delete('/post/:id', (req, res) => {
+  res.send(deletePost(+req.params.id));
 });
 
 server.listen(PORT, () => {
