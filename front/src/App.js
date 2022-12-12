339b6e1e290bf4965a33625632c0613f48b84bc0
rjsej12 <rjsej12@naver.com>
2022-12-13 00:11:15 +0900

    feat: Create routing prototype

--- front/src/App.js
+++ front/src/App.js
@@ -1,3 +1,4 @@
+import { render } from './common';
 import {
   Main, //
   SignIn,
@@ -6,18 +7,45 @@ import {
   Post,
 } from './pages';
 
+const routes = [
+  { path: '/', component: Main },
+  { path: '/signin', component: SignIn },
+  { path: '/signup', component: SignUp },
+  { path: '/edit', component: Edit },
+];
+
+const findComponent = pathname => routes.find(({ path }) => path === pathname).component;
+
 class App {
   render() {
-    const mainPage = new Main().render();
-    const signin = new SignIn().render();
-    const signup = new SignUp().render();
-    const edit = new Edit().render();
-    const post = new Post().render();
+    const RenderComponet = findComponent(window.location.pathname);
+    const page = new RenderComponet().render();
 
     return `
-      ${mainPage}
+      ${page}
     `;
   }
 }
+
+window.addEventListener('click', e => {
+  if (!e.target.matches('a')) return;
+  e.preventDefault();
+  const path = e.target.getAttribute('href');
+  // 현재 페이지와 이동할 페이지가 같으면 이동하지 않는다.
+  if (window.location.pathname === path) return;
+
+  // pushState는 주소창의 url을 변경하지만 HTTP 요청을 서버로 전송하지는 않는다.
+  window.history.pushState(null, null, path);
+  render();
+});
+
+window.addEventListener('popstate', () => {
+  console.log('[popstate]', window.location.pathname);
+  render();
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+  render();
+});
 
 export default App;
