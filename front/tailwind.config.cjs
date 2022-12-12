530763ff83a56e797c37d9b98bd722fd1712c606
HyeonWoo <rmakxh@naver.com>
2022-12-12 19:48:00 +0900

    conf: Set tailwind.config.cjs content option

--- front/tailwind.config.cjs
+++ front/tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.{html,js}'],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
