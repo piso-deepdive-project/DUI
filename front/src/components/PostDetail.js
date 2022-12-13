06ca6052ec1c9098e15933553d3e530a38f73054
rjsej12 <rjsej12@naver.com>
2022-12-13 11:23:37 +0900

    feat: Add base style to PostDetail Component
    PostDetail 컴포넌트 기본 스타일링 및 설정 묶음

--- front/src/components/PostDetail.js
+++ front/src/components/PostDetail.js
@@ -3,10 +3,14 @@ import { Component } from '../common';
 class PostDetail extends Component {
   render() {
     return `
-      <article class="post p4">
-        <h1 class="text-3xl p-4 post-title">제목</h1>
-        <span class="post-description text-sub p-4">작성자 / 날짜</span>
-        <p class="text-lg post-content p-8">
+      <article class="post">
+        <h1 class="post-title">제목</h1>
+        <div class="post-buttons">
+          <button>수정하기</button>
+          <button>삭제하기</button>
+        </div>
+        <span class="post-description">작성자 · 날짜</span>
+        <p class="post-content">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, a soluta maiores amet
           eius beatae debitis qui, ea at ratione exercitationem perferendis corrupti sapiente id
           molestiae distinctio similique, recusandae omnis! Lorem ipsum dolor sit amet consectetur
