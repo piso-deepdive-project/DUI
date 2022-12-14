c3ff5caf3254253e0b1e900f0c830aeed0df2159
PisoDev77 <pisodev0427@gmail.com>
2022-12-14 16:22:04 +0900

    feat: styling postcard
    - style/postcard.css
    - thumbnail.svg  기본 이미지

--- front/src/components/PostCard.js
+++ front/src/components/PostCard.js
@@ -3,14 +3,18 @@ import Component from '../common/Component';
 class PostCard extends Component {
   render() {
     return `
-      <div class="post-card hover:bg-primary" onclick="location.href='/view.html'">
-        <h3 class="text-2 xl">작성자</h3>
-        <span class="text-desc text-sm">날짜 | 시간</span>
-        <span class="text-base">Description dwouhbwoefgnwongOJWDBNOWGWJBG</span>
-        <img class="thumbnail" src="./test2.svg" alt="" />
+      <div class="post-card">
+        <h3 class="card-author">작성자</h3>
+        <span class="card-date">날짜 | 시간</span>
+        <span class="card-description">Description dwouhbwoefgnwongOJWDBNOWGWJBG</span>
+        <img class="thumbnail" src="./thumbnail.svg" alt="" />
       </div>
     `;
   }
+
+  // addEventListener() {
+  //   return [{ type: 'submit', selector: '.post-card', handler: this.validationUser.bind(this) }];
+  // }
 }
 
 export default PostCard;
