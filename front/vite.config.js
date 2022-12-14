aa4664be96c360f0e551b2852d06089eb57c6db6
PisoDev77 <pisodev0427@gmail.com>
2022-12-14 15:36:54 +0900

    fix: Port Number change 6000 -> 3000

--- front/vite.config.js
+++ front/vite.config.js
@@ -3,9 +3,9 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   server: {
     proxy: {
-      '/post': 'http://localhost:6000',
-      '/signin': 'http://localhost:6000',
-      '/signup': 'http://localhost:6000',
+      '/post': 'http://localhost:3000',
+      '/signin': 'http://localhost:3000',
+      '/signup': 'http://localhost:3000',
     },
   },
 });
