77609730397d030363157eee2dae17a67b97a6a7
rjsej12 <rjsej12@naver.com>
2022-12-15 15:50:56 +0900

    refactor: Refactor Components Structure
    App에서는 어떤 Page Component를 부를지만 결정하고, 각 Page
    Component들이 서버에서 필요한 정보를 받아 하위 Component들에게
    전달하도록 변경

--- front/src/pages/Main.js
+++ front/src/pages/Main.js
@@ -1,16 +1,26 @@
+import axios from 'axios';
 import Component from '../common/Component';
 import { MainNav, Posts } from '../components';
 
 class Main extends Component {
-  render() {
-    const { posts } = this.props;
-
+  /**
+   * render가 호출될때 마다 서버로부터 필요한 상태를 받아와서 하위 컴포넌트로 전달
+   * 이를 위해 비동기처리가 필요한 render에 async-await 사용
+   */
+  async render() {
+    const posts = await this.fetchPosts();
     const nav = new MainNav().render();
     const postsString = new Posts({ posts }).render();
     return `
       ${nav}
       ${postsString}
     `;
+  }
+
+  async fetchPosts() {
+    const { data: posts } = await axios.get('/posts');
+
+    return posts;
   }
 }
 export default Main;
