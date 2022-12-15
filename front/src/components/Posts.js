9ea1cf4e4af7e887d2864584423986380031ee58
HyeonWoo <rmakxh@naver.com>
2022-12-15 16:10:19 +0900

    feat: Set MainPage state and props
    - 각 페이지를 root로 사용하기 위해서 state를 각 페이지에 만듦

--- front/src/components/Posts.js
+++ front/src/components/Posts.js
@@ -6,7 +6,11 @@ class Posts extends Component {
   render() {
     const { posts } = this.props;
 
+    // state에서 postFrom와 postFrom을 조작할 handler를 전달한다.
     const postType = new PostType().render();
+
+    // state에서 grid인지 list인지 판별한다.
+    // const postGrid
     const postList = new PostList({ posts }).render();
 
     return `
--- front/src/pages/Main.js
+++ front/src/pages/Main.js
@@ -1,16 +1,33 @@
+import axios from 'axios';
 import Component from '../common/Component';
 import { MainNav, Posts } from '../components';
 
 class Main extends Component {
+  state = {
+    posts: [],
+
+    // list인지 grid인지 판별
+    postForm: 'list',
+  };
+
   render() {
     const { posts } = this.props;
-
     const nav = new MainNav().render();
     const postsString = new Posts({ posts }).render();
     return `
       ${nav}
       ${postsString}
     `;
+  }
+
+  // async fetchPosts() {
+  //   const posts = await axios.get('/posts');
+  // }
+
+  // 해딩 li 클릭시 post의 형식이 list,grid로 바뀐다.
+  togglePostForm(e) {
+    const { postForm } = this.props;
+    this.setState({ postForm });
   }
 }
 export default Main;
