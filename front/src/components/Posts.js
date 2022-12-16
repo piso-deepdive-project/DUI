b973d98925a5486606163075527f6e3396d36905
HyeonWoo <rmakxh@naver.com>
2022-12-16 11:23:41 +0900

    feat: Change postType icon select style

--- front/src/components/Posts.js
+++ front/src/components/Posts.js
@@ -7,7 +7,7 @@ import { Component } from '../common';
 class Posts extends Component {
   async render() {
     const { fetchPosts, setPostType, currentPostType } = this.props;
-    const postType = new PostType({ setPostType }).render();
+    const postType = new PostType({ setPostType, currentPostType }).render();
 
     const postForm =
       currentPostType === 'grid'
