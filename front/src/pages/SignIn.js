50ddf45efea77390178623e9508690048d661528
HyeonWoo <rmakxh@naver.com>
2022-12-19 21:29:25 +0900

    refactor: Signin refactoring
    - 불필요한 필드정의 제거
    - setState 수정
    - 불필요한 함수 제거
    - 추후 에러메시지 수정
    - 스타일 수정

--- front/src/pages/SignIn.js
+++ front/src/pages/SignIn.js
@@ -5,25 +5,21 @@ import { Component } from '../common';
 import userValidation from '../lib/userValidation';
 
 class SignIn extends Component {
-  state = {
-    isValidationUser: false,
-    errMsg: '',
-    emailValue: '',
-  };
-
   email = null;
 
-  password = null;
+  pwd = null;
+
+  userValidation = userValidation();
 
   // prettier-ignore
   render() {
     return `
-      <nav class="user-nav">
-        <a href="/">DUI POST</a>
-      </nav>
+      <header class="user-header">
+        <div class='logo route' data-route="/"></div>
+      </header>
       <form class="signin-form">
         <h1 class="title">SIGNIN</h1>
-        <span class="errorMsg">${this.state.errMsg}</span>
+        <span class="errorMsg">${this.state?.email?.errMsg || this.state?.pwd?.errMsg || ''}</span>
         <div class="signin-container">
           <input
             type="email"
@@ -31,17 +27,19 @@ class SignIn extends Component {
             class="signin-userid"
             minlength="8"
             placeholder="이메일"
-            value="${this.state.emailValue}"
+            value="${this.state?.email?.value ?? ''}"
+            required
           />
           <i class='signin-email-icon icon hidden bx bx-x'></i>
           <input
             type="password"
-            name="password"
-            class="signin-password"
+            name="pwd"
+            class="signin-pwd"
             minlength="5"
             placeholder="비밀번호"
+            required
           />
-          <i class='signin-password-icon icon hidden bx bx-x'></i>
+          <i class='signin-pwd-icon icon hidden bx bx-x'></i>
           <button type="submit"class="signin-btn">로그인</button>
           <button type="button" class="route" data-route="/signup">회원가입</button>
         </div>
@@ -54,50 +52,27 @@ class SignIn extends Component {
     try {
       const signinUser = await axios.post('/signin', { id, pwd });
       if (signinUser !== '') console.log('로그인 성공');
-      // 로그인 성공하고 뒤로가기 클릭시 로그인페이지로 이동 불가
-      window.location.replace('/');
+
+      window.history.pushState(null, null, '/');
+      this.setState();
     } catch (error) {
       console.log(error);
       this.setState({ errMsg: error.response.data.err });
     }
   }
 
-  setuserValidation(e) {
-    // input값을 입력하고 submit 하면 해당 값이 조건에 맞는지 검사해야한다.
-    [...e.target.querySelectorAll('.signin-container input')].forEach($input => {
-      this[$input.name] = $input;
-
-      userValidation[$input.name].value = $input.value;
-    });
-  }
-
-  // 제대로 입력하지 않은 값이 존재하면 email부터 차례대로 focus가 이동한다.
-  moveFocus() {
-    if (!userValidation.email.valid) this.email.focus();
-    else if (!userValidation.password.valid) this.password.focus();
-  }
-
-  // 제대로 입력하지 않은 값이 존재하면 errorMsg가 출력된다.
-  editErrorMsg() {
-    if (!userValidation.email.valid) return userValidation.email.error;
-    if (!userValidation.password.valid && userValidation.password.value !== '') return userValidation.password.error;
-    return '';
-  }
-
   validationUser(e) {
     e.preventDefault();
 
-    this.setuserValidation(e);
-    this.setState({
-      isValidationUser: userValidation.signinValid,
-      errMsg: this.editErrorMsg(),
-      emailValue: this.email.value,
-    });
-    this.moveFocus();
-
-    if (this.state.isValidationUser) {
-      this.getUser(userValidation.email.value, userValidation.password.value);
+    if (this.userValidation.signinValid) {
+      this.getUser(this.userValidation.email.value, this.userValidation.pwd.value);
     }
+
+    const signinForm = e.target;
+    this.setState({
+      email: this.userValidation.email.valid(signinForm.email.value),
+      pwd: this.userValidation.pwd.valid(signinForm.pwd.value),
+    });
   }
 
   deleteInputValue(e) {
