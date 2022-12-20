375283a6bc22668af266c94a34fe6c04bcb6c092
HyeonWoo <rmakxh@naver.com>
2022-12-20 16:32:55 +0900

    refactor: Edit page refactoring
    - 클릭이벤트로 동작하는 addPost와 updatePost를 submit이벤트로 동작하게 변경
    - render에서 두개의 템플릿을 하나로 합침
    - 불필요한 click이벤트 삭제

--- front/src/pages/Edit.js
+++ front/src/pages/Edit.js
@@ -3,130 +3,53 @@ import axios from 'axios';
 import { Component } from '../common';
 
 class Edit extends Component {
+  // prettier-ignore
   async render() {
     const pathId = window.location.pathname.split('/')[2];
+    let setPost = null;
 
     if (pathId) {
-      const { data: post } = await axios.get(`/post/${+pathId}`);
-      const {
-        title, //
-        tags,
-        content,
-        id,
-      } = post.post;
+      const { data: { post } } = await axios.get(`/post/${+pathId}`);
+      setPost = post;
+    }
 
-      return `
-      <form class="edit" data-id="${id}">
-        <input type="text" class="edit-title" placeholder="제목을 입력하세요" required value="${title}" />
+    return `
+      <form class="edit">
+        <input type="text" name="title" class="edit-title" placeholder="제목을 입력하세요" required value="${pathId ? setPost.title : ''}" />
         <div class="tag">
-          ${tags?.map(tag => `<span class="tag-box">${tag}</span>`).join('')}
+          ${pathId ? setPost.tags?.map(tag => `<span class="tag-box">${tag}</span>`).join('') : ''}
           <input
+            name="tag"
             type="text"
             class="edit-tag"
             placeholder="태그를 입력하세요"
           />
           <div class="edit-tag-description">
             <span>스페이스바를 입력하여 태그를 등록할 수 있습니다.</span>
-            <span>등록된 태그를 클릭하면 삭제됩니다.</span>
+            <span>등록된 태그를 클릭하거나 백스페이스를 입력하면 삭제됩니다.</span>
           </div>
         </div>
-        <textarea
-          class="edit-post"
-          cols="30"
-          rows="10"
-          placeholder="Draw Your Idea"
-          required          
-        >${content}</textarea>
-        <div class="edit-buttons">
-          <button class="edit-return">뒤로가기</button>
-          <button class="edit-update route" data-route="/post/${id}">수정하기</button>
-        </div>
-      </form>
-    `;
-    }
-
-    return `
-      <form class="edit">
-        <input type="text" class="edit-title" placeholder="제목을 입력하세요" required />
-        <div class="tag">
-          <input
-            type="text"
-            class="edit-tag"
-            placeholder="태그를 입력하세요"
-          />
-        </div>
-        <div class="edit-tag-description">
-        <span>스페이스바를 입력하여 태그를 등록할 수 있습니다.</span>
-        <span>등록된 태그를 클릭하면 삭제됩니다.</span>
-      </div>
         <textarea
           name="area"
           class="edit-post"
           cols="30"
           rows="10"
           placeholder="Draw Your Idea"
-          required></textarea>
+          required          
+        >${pathId ? setPost.content : ''}</textarea>
         <div class="edit-buttons">
           <button type="button" class="edit-return">뒤로가기</button>
-          <button type="button" class="edit-add">작성하기</button>
+          <button class="edit-${pathId ? 'update' : 'add'}">${pathId ? '수정하기' : '작성하기'}</button>
         </div>
       </form>
     `;
   }
 
-  async addPost(e) {
-    const title = document.querySelector('.edit-title').value;
-    const content = document.querySelector('.edit-post').value;
-    const tags = [...document.querySelectorAll('.tag-box')].map($span => $span.textContent.trim());
-
-    if (title.trim() === '' || content.trim() === '') {
-      e.stopPropagation();
-      return;
-    }
-
-    await axios.post('post', {
-      title,
-      tags,
-      content,
-      date: new Date(),
-    });
-
-    window.history.pushState(null, null, '/');
-    this.setState();
-  }
-
-  async updatePost(e) {
-    const title = document.querySelector('.edit-title').value;
-    const content = document.querySelector('.edit-post').value;
-    const tags = [...document.querySelectorAll('.tag-box')].map($span => $span.textContent.trim());
-
-    if (title.trim() === '' || content.trim() === '') {
-      e.stopPropagation();
-      return;
-    }
-
-    const id = +e.target.closest('.edit').dataset.id;
-
-    await axios.patch('/post', {
-      id,
-      title: title.value,
-      tags,
-      content,
-      date: new Date(),
-    });
-    window.history.pushState(null, null, '/');
-    this.setState();
-  }
-
   moveFocus(e) {
     const editForm = document.querySelector('.edit');
-    const [title, tag] = document.querySelectorAll('.edit input');
 
-    if (e.target === title) tag.focus();
-    else {
-      editForm.area.focus();
-      e.preventDefault();
-    }
+    if (e.target === editForm.title) editForm.tag.focus();
+    else editForm.area.focus();
   }
 
   createTagBox(e) {
@@ -142,6 +65,7 @@ class Edit extends Component {
     if (e.isComposing || e.keyCode === 229) return;
     if (e.key === 'Enter' && !e.target.matches('.edit-post')) {
       this.moveFocus(e);
+      e.preventDefault();
     }
 
     if (e.target.matches('.edit-tag') && e.key === 'Backspace' && e.target.value.trim() === '') {
@@ -149,7 +73,7 @@ class Edit extends Component {
     }
   }
 
-  inputDescription(e) {
+  tagInputHandeler(e) {
     if (
       (e.target.matches('.edit-tag') && e.target.value.trim() !== '') ||
       e.target.parentNode.querySelector('.tag-box') !== null
@@ -163,17 +87,47 @@ class Edit extends Component {
     if (e.target.value.slice(-1) === ' ' && e.target.value.trim() !== '') this.createTagBox(e);
   }
 
-  preventSubmit(e) {
+  async submitPost(e) {
     e.preventDefault();
+
+    const pathId = window.location.pathname.split('/')[2];
+
+    const title = e.target.title.value;
+    const content = e.target.area.value;
+    const tags = [...e.target.querySelectorAll('.tag-box')].map($span => $span.textContent.trim());
+
+    if (pathId) {
+      await axios.patch('/post', {
+        id: +pathId,
+        title,
+        tags,
+        author: { name: 'user' },
+        content,
+        date: new Date(),
+      });
+      window.history.pushState(null, null, `/post/${pathId}`);
+    } else {
+      await axios.post('/post', {
+        title,
+        tags,
+        author: { name: 'user' },
+        content,
+        date: new Date(),
+      });
+      window.history.pushState(null, null, '/');
+    }
+    this.setState();
   }
 
-  clickTag(e) {
+  deleteTag(e) {
     if (e.target.matches('.tag')) e.target.querySelector('.edit-tag').focus();
     if (e.target.matches('.tag-box')) e.target.remove();
   }
 
-  showTagDescription() {
-    document.querySelector('.edit-tag-description').style.visibility = 'visible';
+  showTagDescription(e) {
+    if (e.target.parentNode.querySelector('.tag-box') === null) {
+      document.querySelector('.edit-tag-description').style.visibility = 'visible';
+    }
   }
 
   hideTagDescription() {
@@ -185,23 +139,12 @@ class Edit extends Component {
     window.history.back();
   }
 
-  /**
-   * keydown: 엔터 -> 포커스 이동, 스페이스바 -> 태그박스 생성, 백스페이스바 -> 태그박스 제거
-   * input: description 생성및 제거
-   * submit: submit의 기본동작을 막음.
-   * click-tag: 카테고리박스 클릭시 제거 , 포커스 edit-category로 이동
-   * click-edit-return: 뒤로가기 클릭시 이전페이지로 이동
-   * focusin: description 생성
-   * focusout: description 제거
-   */
   addEventListener() {
     return [
       { type: 'keydown', selector: '.edit', handler: this.keydownHandeler.bind(this) },
-      { type: 'input', selector: '.edit-tag', handler: this.inputDescription.bind(this) },
-      { type: 'submit', selector: '.edit', handler: this.preventSubmit },
-      { type: 'click', selector: '.edit-add', handler: this.addPost.bind(this) },
-      { type: 'click', selector: '.edit-update', handler: this.updatePost.bind(this) },
-      { type: 'click', selector: '.tag', handler: this.clickTag },
+      { type: 'input', selector: '.edit-tag', handler: this.tagInputHandeler.bind(this) },
+      { type: 'submit', selector: '.edit', handler: this.submitPost.bind(this) },
+      { type: 'click', selector: '.tag', handler: this.deleteTag },
       { type: 'click', selector: '.edit-return', handler: this.moveeBackPage },
       { type: 'focusin', selector: '.edit-tag', handler: this.showTagDescription },
       { type: 'focusout', selector: '.edit-tag', handler: this.hideTagDescription },
