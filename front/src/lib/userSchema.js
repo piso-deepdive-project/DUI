34974d1d62d39095583d08d53bdeefecf062c87d
PisoDev77 <pisodev0427@gmail.com>
2022-12-21 13:35:58 +0900

    refactor: Signin
    - ....

---
+++ front/src/lib/userSchema.js
@@ -0,0 +1,81 @@
+const userSchema = {
+  requireMsg: '이메일을 입력하세요..',
+
+  email: {
+    value: '',
+    isValid: email => /[a-zA-Z0-9]+@[a-z]+\.[a-z]{2,3}/i.test(email) && /.{6,12}/.test(email),
+    _valid: false,
+
+    valid(email) {
+      this.value = email;
+      this._valid = this.isValid(this.value);
+
+      return {
+        value: this.value,
+        isErr: this._valid,
+        errMsg: email === '' ? userSchema.requireMsg : this._valid ? '' : this.error,
+      };
+    },
+    error: '이메일은 영문,숫자인 이메일 형식만 가능합니다.',
+  },
+
+  authorname: {
+    value: '',
+    isValid: authorname => /^[ㄱ-ㅎ|가-힣|a-z|A-Z|0-9|]{2,5}$/i.test(authorname),
+
+    _valid: false,
+    valid(authorname) {
+      this.value = authorname;
+      this._valid = this.isValid(this.value);
+
+      return {
+        value: this.value,
+        errMsg: authorname === '' ? userSchema.requireMsg : this._valid ? '' : this.error,
+      };
+    },
+
+    error: '한글,영문,숫자 2~5자로 구성하세요',
+  },
+
+  pwd: {
+    value: '',
+    isValid: pwd => /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{6,12}$/g.test(pwd),
+    _valid: false,
+
+    valid(pwd) {
+      this.value = pwd;
+      this._valid = this.isValid(pwd);
+
+      return {
+        isErr: this._valid,
+        errMsg: this._valid ? '' : this.error,
+      };
+    },
+
+    error: '비밀번호는 영문,숫자,특수문자를 조합하여(하나씩포함) 6~12자로 구성하세요.',
+  },
+
+  pwd2: {
+    isValid: pwd2 => userSchema.pwd.value === pwd2,
+    _valid: false,
+
+    valid(pwd2) {
+      this._valid = this.isValid(pwd2);
+      return {
+        errMsg: pwd2 === '' ? userSchema.requireMsg : this._valid ? '' : this.error,
+      };
+    },
+
+    error: '비밀번호가 일치하지 않습니다.',
+  },
+
+  get signupValid() {
+    return userSchema.email._valid && userSchema.authorname._valid && userSchema.pwd._valid && userSchema.pwd2._valid;
+  },
+
+  get signinValid() {
+    return userSchema.email._valid && userSchema.pwd._valid;
+  },
+};
+
+export default userSchema;
