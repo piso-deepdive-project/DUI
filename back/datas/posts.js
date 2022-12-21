4f1079d5d73bf71f5e12765cdeecb7bcb31a72e3
PisoDev77 <pisodev0427@gmail.com>
2022-12-21 12:01:49 +0900

    refactor: back/index.js
    - API를 api별로 묶고
    - 파일 분리
    - 문서 작성

--- back/datas/posts.js
+++ back/datas/posts.js
@@ -1,14 +1,4 @@
-let users = [
-  {
-    id: 'test123@gmail.com',
-    pwd: 'utzazz12!!',
-    name: 'Uta',
-    posts: [0],
-    likes: [],
-  },
-];
-
-let posts = [
+const posts = [
   {
     id: 0,
     title: 'NAS - Nas Is Like',
@@ -78,91 +68,6 @@ let posts = [
   },
 ];
 
-// USER
-const getUser = user => users.find(_usr => _usr.id === user.id && _usr.pwd === user.pwd);
-
-const addUser = user => {
-  users = [...users, user];
-};
-
-const updateUser = user => {
-  users = users.map(_usr => (_usr.id === user.id ? user : _usr));
-};
-
-const deleteUser = id => {
-  users = users.filter(_usr => _usr.id !== id);
-};
-
-const isUniqueId = id => {
-  if (users.length === 0) return true;
-  return users.some(user => user.id === id);
-};
-
-// prettier-ignore
-const addLike = (userId, postId) => {
-  users = users.map(user => (user.id === userId
-    ? {
-      ...user,
-      likes: user.likes.includes(postId)
-        ? user.likes.filter(like => like !== +postId) : [postId, ...user.likes]
-    }
-    : user));
-};
-
-const getLikes = userId => users.find(user => user.id === userId).likes;
-
-// POST
-const getPosts = ({ id, pageSize }) => {
-  const _posts = [];
-  let i = id;
-  let cnt = pageSize;
-
-  while (posts[i] && cnt >= 0) {
-    _posts.push(posts[i]);
-    i += 1;
-    cnt -= 1;
-  }
-
-  return _posts;
-};
-
-// const getPost = id => posts.filter(post => post.id === id);
-const getPost = id => posts.find(post => post.id === id);
-
-const getNextId = () => Math.max(...posts.map(post => post.id), 0) + 1;
-
-const addPost = post => {
-  const newPost = { ...post, id: getNextId() };
-  posts = [...posts, newPost];
-};
-
-const updatePost = post => {
-  posts = posts.map(_post => (_post.id === +post.id ? { ..._post, ...post } : _post));
-  return getPost(+post.id);
-};
-
-const deletePost = id => {
-  posts = posts.filter(post => post.id !== id);
-  return getPost(id);
-};
-
-const addComment = ({ postId, comment, author }) => {
-  const newComment = { comment, author, date: new Date() };
-  posts = posts.map(_post => (_post.id === postId ? { ..._post, comments: [..._post.comments, newComment] } : _post));
-};
-
 module.exports = {
-  getUser,
-  addUser,
-  updateUser,
-  deleteUser,
-  isUniqueId,
-  addLike,
-  getLikes,
-  getPosts,
-  getPost,
-  addPost,
-  updatePost,
-  deletePost,
-  addComment,
+  posts,
 };
